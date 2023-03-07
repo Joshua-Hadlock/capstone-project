52b4561ce5b572734be46311ad34b2b084d26f5c
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro-2.local>
2023-03-07 09:01:39 -0700

    styling dashboard

--- client/src/pages/userDashboard.js
+++ client/src/pages/userDashboard.js
@@ -1,5 +1,25 @@
 import NavBar from "../components/navbar"
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+
 export default function Dashboard() {
+    const [classes, setClasses] = useState(null);
+
+    const getYourClasses = () => {
+        Axios({
+          method: "GET",
+          withCredentials: true,
+          url: "/allYourClasses",
+        }).then((res) => {
+          setClasses(res.data)
+        })
+      }
+
+    //   useEffect(() => {
+    //     getYourClasses()
+    //   }, [])
+
     return(
         <div class="body">
             <NavBar />
@@ -17,7 +37,28 @@ export default function Dashboard() {
                 </ul>
             </div>
             <div id="courses">
+            <h1>Your Classes </h1>
+                <div class="getClasses">
+                    {/* <button onClick={getYourClasses}>Submit</button>
+                    {classes ? <h1>Your Classes <ul>{classes.map((item)=><li key={item._id}>{item.title}</li>)}</ul></h1> : null} */}
+                    <div class="class">
+                        <h1>Random Class 1</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus faucibus scelerisque eleifend donec.</p>
+                    </div>
+ 
+                    <div class="class">
+                        <h1>Random Class 2</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus faucibus scelerisque eleifend donec.</p>
+                    </div>
+                    <div class="class">
+                        <h1>Random Class 2</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus faucibus scelerisque eleifend donec.</p>
+                    </div>
+                    
+                    
+                </div>
                 
+
             </div>
             <div id="addCourses">
                 
