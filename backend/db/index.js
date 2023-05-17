c471a6a7419cf5567573c6294d98011da8ca4f9a
Joshua Hadlock <jhadlock@Wades-MacBook-Pro.local>
2023-05-17 07:43:24 -0600

    final push

--- backend/db/index.js
+++ backend/db/index.js
@@ -44,7 +44,7 @@ exports.getUserById = async (id) => {
 }
 exports.getLoginUser = async (req, res) => {
     pool.query(`select * from users where id = $1`, [res.locals.user.id], (err, results) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         res.status(200).json(results.rows[0]);
     })
 }
@@ -58,7 +58,7 @@ exports.register = async (req, res) => {
 
 exports.getAllClasses = async (req, res) => {
     pool.query(`select * from courses`, (err, results) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         for (let row of results.rows) {
         }
         res.status(200).json(results.rows);
@@ -67,7 +67,7 @@ exports.getAllClasses = async (req, res) => {
 
 exports.getOneClass = async (req, res) => {
     pool.query(`select * from courses where id = $1`, [req.body.selectedOneCourse], (err, results) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         for (let row of results.rows) {
         }
         res.status(200).json(results.rows);
@@ -79,14 +79,14 @@ exports.getOneClass = async (req, res) => {
 exports.requestAddableClasses = async (req, res) => {
     pool.query(`select *, case when users_courses.cnt is null then 0 else users_courses.cnt end as cnt2 into tempCourses from courses
     left join(select courses_id, COUNT(courses_id) as cnt from user_course Group By courses_id) as users_courses on courses.id = users_courses.courses_id;` , (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         pool.query(`ALTER TABLE tempCourses
     DROP COLUMN cnt;`, (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         pool.query(`
     select * from tempCourses
     where id not in (select courses_id from user_course where users_id = $1) and maximum_capacity > cnt2;`, [res.locals.user.id], (err, results) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         for (let row of results.rows) {
             console.log(JSON.stringify(row));
         }
@@ -113,27 +113,27 @@ exports.addStudentClass = async (req, res) => {
         values
         ((select id from courses where id = $1), (select id from users where id = $2), (select credit_hours from courses where id = $1), (select tuition_cost from courses where id = $1))
         `, [req.body.selectedClass, res.locals.user.id], (err, results) => {
-        if (err) throw err;
-        return('oof')
+        if (err) return('oof');
+        
     })
     }
 }
 
 exports.removeStudentClass = async (req, res) => {
     pool.query(`delete from user_course where courses_id = $1 and users_id = $2`, [req.body.selectedClass, res.locals.user.id], (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
     })
 }
 
 exports.deleteUser = async (req, res) => {
     pool.query(`delete from users where id = $1`, [req.body.selectedUser], (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
     })
 }
 
 exports.getAllYourClasses = async (req, res) => {
     pool.query(`select * from courses where id in (select courses_id from user_course where users_id = $1)`, [res.locals.user.id], (err, results) => {
-        if (err) throw err;
+        if (err) console.log('oof');
         console.log(results.rows)
         res.status(200).json(results.rows);
     })
@@ -142,18 +142,18 @@ exports.getAllYourClasses = async (req, res) => {
 exports.createClass = async (req, res) => {
     const data = req.body;
     pool.query(`insert into days_time (courses_id, schedule, classroom_number) values ($1, $2, $3)`, [data.id, data.schedule, data.classroom_number], (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
     })
     pool.query(`insert into courses (id, title, description, schedule, classroom_number, maximum_capacity, credit_hours, tuition_cost) values ($1, $2, $3, $4, $5, $6, $7, $8)`, [data.id, data.title, data.description, data.schedule, data.classroom_number, data.maximum_capacity, data.credit_hours, data.tution_cost], (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
     })
 }
 
 exports.deleteClass = async (req, res) => {
     pool.query(`delete from user_course where courses_id = $1`, [req.body.selectedClass], (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
     })
     pool.query(`delete from courses where id = $1`, [req.body.selectedClass], (err) => {
-        if (err) throw err;
+        if (err) console.log('oof');
     })
 }
